74a9ce06a9e8c9431384de5109fb4eb1651a88f9
Michail Romaniuk <104836585+M4ckl@users.noreply.github.com>
2025-11-10 17:59:03 +0100

    Initial Commit

---
+++ UniversityApp/UniversityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityAppApp.swift
+//  UniversityApp
+//
+//  Created by Misha on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct UniversityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniversityAppTests/UniversityAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityAppTests.swift
+//  UniversityAppTests
+//
+//  Created by Misha on 10/11/2025.
+//
+
+import Testing
+@testable import UniversityApp
+
+struct UniversityAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
